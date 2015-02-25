348631a453a1f816353ce0e98142f64de3f62c7c
Hector Fabio Cadavid Rengifo <hcadavid@Hectors-MacBook-Pro.local>
2015-02-25 17:04:32 -0500

    first commit

---
+++ src/test/java/aaaaa/NewEmptyJUnitTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aaaaa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void aaa(){
+        assertTrue(false);
+    }
+    
+}
