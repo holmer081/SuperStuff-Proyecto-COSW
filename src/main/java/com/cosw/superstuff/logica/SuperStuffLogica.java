cdc9807c81857c44a77fa118006116ad6901041e
holmer081 <holmer081@gmail.com>
2015-03-04 06:49:55 -0500

    Agregado cascaron de los metodos que va a ofrecer la capa logica

--- src/main/java/com/cosw/superstuff/logica/SuperStuffLogica.java
+++ src/main/java/com/cosw/superstuff/logica/SuperStuffLogica.java
@@ -5,10 +5,65 @@
  */
 package com.cosw.superstuff.logica;
 
+import com.cosw.superstuff.rep.RepositorioEnvios;
+import com.cosw.superstuff.rep.RepositorioProductos;
+import com.cosw.superstuff.rep.RepositorioProveedores;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author HOMERO
  */
+@Service
 public class SuperStuffLogica {
+    @Autowired
+    private RepositorioProveedores repositorioProveedores;
     
+    @Autowired
+    private RepositorioProductos repositorioProductos;
+    
+    @Autowired
+    private RepositorioEnvios repositorioEnvios;
+    
+    
+    public void registrarProducto(){
+        
+    }
+    
+    public void cargarProductosPorCategoria(){
+        
+    }
+    
+    public void cargarTodosLosProductos(){
+        
+    }
+    
+    public void cargarProductosPorProveedor(){
+        
+    }
+    
+    public void cargarCategorias(){
+        
+    }
+    
+    public void registrarPedido(){
+        
+    }
+    
+    public void registrarEnvio(){
+        
+    }
+    
+    public void actualizarEstadoDeEnvio(){
+        
+    }
+    
+    public void crearNuevoProveedor(){
+        
+    }
+    
+    public void crearNuevoTenedreo(){
+        
+    }
 }
