b6a51fc3a3416a14f46a651693d31be0e1716d49
Azurebird <white.camilo@gmail.com>
2015-03-09 21:18:55 -0500

    Peque;os cambios en el contex.xml y el applicationContext.xml

--- src/main/java/com/cosw/superstuff/rest/pedido/RestPedido.java
+++ src/main/java/com/cosw/superstuff/rest/pedido/RestPedido.java
@@ -5,7 +5,11 @@
  */
 package com.cosw.superstuff.rest.pedido;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,4 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pedido")
 public class RestPedido {
     
+    @RequestMapping(value="/echo/{input}",method = RequestMethod.GET)        
+    public ResponseEntity<?> consultaX(@PathVariable String input) {       
+        return new ResponseEntity<>("REST API working. Echo:"+input,HttpStatus.ACCEPTED);
+    }
 }
