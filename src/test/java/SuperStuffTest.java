5f04825b7aee59b8da3d6536c01a9e3dad1dc134
holmer081 <holmer081@gmail.com>
2015-03-10 20:45:49 -0500

    Creado paquete para realizar pruebas

---
+++ src/test/java/SuperStuffTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.cosw.superstuff.logica.SuperStuffLogica;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author HOMERO
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContextH2.xml"})
+public class SuperStuffTest {
+    
+    @Autowired
+    private SuperStuffLogica superStuff;
+    
+    public SuperStuffTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void registrarProveedorTest(){
+        //superStuff.crearNuevoProveedor(idProveedores, null, null, null, null, null, null)
+    }
+}
